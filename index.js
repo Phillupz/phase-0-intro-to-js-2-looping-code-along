aebd6a21ea8dbbc52cf950f08e46742106f8cbf6
Phillupz <phillup.zukowski@gmail.com>
2022-03-16 13:25:06 -0400

    commit tree 61095b167aea8ff6b852c3154d2f5478896db29f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+function writeCards(names, surprise) {
+    let newNames = []
+
+    for (let i = 0; i < names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`)
+    }
+
+return newNames;
+
+}
+let n = 10
+function countDown(n) {
+    while (n >= 0){ 
+        console.log(n--)
+    }
+return n
+}
